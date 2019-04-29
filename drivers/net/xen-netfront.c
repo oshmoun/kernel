0a906af8aa8ebef09094f5877c6c0dd858a87f02
oshmoun <oshmoun100@googlemail.com>
2019-04-30 00:57:24 +0200

    Revert "treewide: Linux 4.9.130"
    This reverts commit dfbd2575f04edc7412fd5a90980018b52719c5c9.

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -906,11 +906,7 @@ static RING_IDX xennet_fill_frags(struct netfront_queue *queue,
 			BUG_ON(pull_to <= skb_headlen(skb));
 			__pskb_pull_tail(skb, pull_to - skb_headlen(skb));
 		}
-		if (unlikely(skb_shinfo(skb)->nr_frags >= MAX_SKB_FRAGS)) {
-			queue->rx.rsp_cons = ++cons;
-			kfree_skb(nskb);
-			return ~0U;
-		}
+		BUG_ON(skb_shinfo(skb)->nr_frags >= MAX_SKB_FRAGS);
 
 		skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags,
 				skb_frag_page(nfrag),
@@ -1047,8 +1043,6 @@ err:
 		skb->len += rx->status;
 
 		i = xennet_fill_frags(queue, skb, &tmpq);
-		if (unlikely(i == ~0U))
-			goto err;
 
 		if (rx->flags & XEN_NETRXF_csum_blank)
 			skb->ip_summed = CHECKSUM_PARTIAL;
