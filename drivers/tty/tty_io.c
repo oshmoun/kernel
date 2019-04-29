731e6118dcf1940d5f4ac7502c69a0d3f3d7aff2
oshmoun <oshmoun100@googlemail.com>
2019-04-30 00:57:14 +0200

    Revert "treewide: Linux 4.9.152"
    This reverts commit fcf53bbfe3d3eb886861eba43a5fd13c478fb0ce.

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1475,8 +1475,7 @@ static void tty_driver_remove_tty(struct tty_driver *driver, struct tty_struct *
 static int tty_reopen(struct tty_struct *tty)
 {
 	struct tty_driver *driver = tty->driver;
-	struct tty_ldisc *ld;
-	int retval = 0;
+	int retval;
 
 	if (driver->type == TTY_DRIVER_TYPE_PTY &&
 	    driver->subtype == PTY_TYPE_MASTER)
@@ -1488,21 +1487,14 @@ static int tty_reopen(struct tty_struct *tty)
 	if (test_bit(TTY_EXCLUSIVE, &tty->flags) && !capable(CAP_SYS_ADMIN))
 		return -EBUSY;
 
-	ld = tty_ldisc_ref_wait(tty);
-	if (ld) {
-		tty_ldisc_deref(ld);
-	} else {
-		retval = tty_ldisc_lock(tty, 5 * HZ);
-		if (retval)
-			return retval;
+	tty->count++;
 
-		if (!tty->ldisc)
-			retval = tty_ldisc_reinit(tty, tty->termios.c_line);
-		tty_ldisc_unlock(tty);
-	}
+	if (tty->ldisc)
+		return 0;
 
-	if (retval == 0)
-		tty->count++;
+	retval = tty_ldisc_reinit(tty, tty->termios.c_line);
+	if (retval)
+		tty->count--;
 
 	return retval;
 }
